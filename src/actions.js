f45602647144569d8b95c776d9e812c9fa6840f5
Mehmet Kologlu <mkologlu@mobiquityinc.com>
2018-02-13 10:58:18 +0100

    Add data actions

---
+++ src/actions.js
@@ -0,0 +1,25 @@
+import { FETCHING_DATA, FETCHING_DATA_SUCCESS, FETCHING_DATA_FAILURE } from './constants';
+import getPeople from './api';
+
+export function getData() {
+  return {
+    type: FETCHING_DATA
+  };
+}
+
+export function getDataSuccess(data) {
+  return {
+    type: FETCHING_DATA_SUCCESS,
+    data,
+  };
+}
+
+export function getDataFailure() {
+  return {
+    type: FETCHING_DATA_FAILURE
+  };
+}
+
+export function fetchData(httpClient) {
+  return {};
+}
